57613a33c55a5cc402df072c036851efd3c2f709
Dreenvski332 <m.stanulewicz332@gmail.com>
2024-06-07 23:59:17 +0200

    changed pathfactory to work with new user

--- GreenOffice/PathFactory.cs
+++ GreenOffice/PathFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,8 @@ namespace GreenOffice
 {
     public class PathFactory
     {
-        public string connString = @"C:\Users\Mikołaj Stanulewicz\Desktop\GreenOffice\settings\connection.txt"; //this is the combined string used to connect to DB
-        static string pathString = @"C:\Users\Mikołaj Stanulewicz\Desktop\GreenOffice\settings\"; //this sets path to pathFactory files, it's just easier to change in needed
+        public string connString = @"C:\Users\ms\Desktop\GreenOffice\settings\connection.txt"; //this is the combined string used to connect to DB
+        static string pathString = @"C:\Users\ms\Desktop\GreenOffice\settings\"; //this sets path to pathFactory files, it's just easier to change in needed
         public string sourceString = pathString + "source.txt"; //source so like: 127.0.0.1
         public string portString = pathString + "port.txt"; // port should look like this: 3306
         public string usernameString = pathString + "username.txt"; //username for the database: root
